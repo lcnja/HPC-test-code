e621865ec194ee9530f42b051dfe9316db5d0a56
John Rugis <j.rugis@auckland.ac.nz>
2013-10-04 15:59:39 +1300

    Updated files.

--- H0004/H.cpp
+++ H0004/H.cpp
@@ -1,24 +1,18 @@
 #include "mpi.h"
-#include <cstdio>
+#include <unistd.h>
+#include <iostream>
 #include <string>
-#include <random>
 
-// execute system command and return response
-std::string sys_command(const char command[]){
-  char result[80];
-  FILE *in;
-  in = popen(command, "r");
-  fgets(result, sizeof(result), in);
-  pclose(in);
-  return std::string(result);
-}
+using namespace std;
 
 // initialize mpi
 void mpi_init(int argc, char* argv[], int *mpi_rank, int *mpi_size){
+  char buf[64];
+  gethostname(buf, sizeof(buf));
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, mpi_rank);
   MPI_Comm_size(MPI_COMM_WORLD, mpi_size);
-  printf("%d %s", mpi_rank, sys_command("hostname").c_str());
+  cout << *mpi_rank << " " << string(buf) << endl;
 }
 
 // shutdown mpi
@@ -29,17 +23,9 @@ void mpi_shutdown(){
 int main(int argc, char *argv[])
 {
   int mpi_rank, mpi_size;
-  double data[1000];
 
   mpi_init(argc, argv, &mpi_rank, &mpi_size);
-
-  std::mt19937 generator(1729);
-  std::uniform_real_distribution<double> distribution(0.0,1.0);
-  for(size_t i = 0; i < sizeof(data); i++){
-    data[i] = distribution(generator);
-  }
 
   mpi_shutdown();
   return 0;
 }
-
--- H0008/H.cpp
+++ H0008/H.cpp
@@ -1,24 +1,18 @@
 #include "mpi.h"
-#include <cstdio>
+#include <unistd.h>
+#include <iostream>
 #include <string>
 
 using namespace std;
 
-// execute system command and return response
-string sys_command(const char command[]){
-  char result[80];
-  FILE *in;
-  in = popen(command, "r");
-  fgets(result, sizeof(result), in);
-  pclose(in);
-  return string(result);
-}
-
 // initialize mpi
 void mpi_init(int argc, char* argv[], int *mpi_rank, int *mpi_size){
+  char buf[64];
+  gethostname(buf, sizeof(buf));
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, mpi_rank);
   MPI_Comm_size(MPI_COMM_WORLD, mpi_size);
+  cout << *mpi_rank << " " << string(buf) << endl;
 }
 
 // shutdown mpi
@@ -28,15 +22,10 @@ void mpi_shutdown(){
 
 int main(int argc, char *argv[])
 {
-  string host_name;
   int mpi_rank, mpi_size;
 
-  host_name = sys_command("hostname");
   mpi_init(argc, argv, &mpi_rank, &mpi_size);
-
-  printf("%d %s", mpi_rank, host_name.c_str());
 
   mpi_shutdown();
   return 0;
 }
-
